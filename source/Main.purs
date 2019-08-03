405bdc6a7460c25c36d643952cf48da6c02ed336
Scott Chu <scottchu@icloud.com>
2019-08-03 14:52:02 -0400

    complete chapter 2

--- source/Main.purs
+++ source/Main.purs
@@ -2,8 +2,16 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal width height = do
+  sqrt (width * width + height * height)
+
+circleArea :: Number -> Number
+circleArea radius = pi * (sqrt radius)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  logShow (diagonal 3.0 4.0)
